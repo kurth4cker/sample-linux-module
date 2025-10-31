d9d0e330af3852b5dd3e892e5147295c21a3d5fe
kurth4cker <kurth4cker@gmail.com>
2025-10-31 20:03:54 +0300

    kurth4cker: walk all the tasks

--- kurth4cker.c
+++ kurth4cker.c
@@ -4,11 +4,29 @@
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("I'm pörfek hacker men.");
 
+static void walk_processes(void) {
+	struct task_struct *ts;
+	struct list_head *lh;
+
+	rcu_read_lock();
+
+	lh = rcu_dereference(init_task.tasks.next);
+	while (lh != &init_task.tasks) {
+		ts = container_of(lh, struct task_struct, tasks);
+		printk(KERN_INFO "kurth4cker: PID = %d, COMM = %s", ts->pid, ts->comm);
+		lh = rcu_dereference(ts->tasks.next);
+	}
+
+	rcu_read_unlock();
+}
+
 int kurth4cker_init(void);
 void kurth4cker_exit(void);
 
 int kurth4cker_init(void) {
 	printk(KERN_INFO "kurth4cker: hello world\n");
+
+	walk_processes();
 
 	return 0;
 }
